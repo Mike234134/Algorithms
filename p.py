fee2aff8395d19aee6c9e18bf8261b90d206512d
Mike234134 <130918811+Mike234134@users.noreply.github.com>
2024-08-14 11:42:30 -0700

    my code

---
+++ p.py
@@ -0,0 +1,10 @@
+def checkNearbyDates(targetDate,datesList):
+    if targetDate[2] != date[2] or targetDate[1] != date[1]:
+        return(False)
+    if targetDate[4]  + "30" in Date[3]+ Date[4](range(1,31)):
+        return(True)
+    return(False)
+    
+
+    
+    checkNearbyDates()
